24a139ba2f4cab37ee2c93c93b511cb1217145c4
awaknez <awaknez@users.noreply.github.com>
2026-01-25 13:28:51 +0900

    Enable YAML aliases early to fix Psych error on Ruby 3.2

--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,18 @@
 require 'logger'  # ← 2025/06/29これを最上部に追加
 
+# --- Psych(YAML) alias enable for Ruby 3.2 ---
+require "psych"
+module Psych
+  class << self
+    alias_method :_orig_safe_load, :safe_load
+    def safe_load(yaml, **kwargs)
+      kwargs[:aliases] = true unless kwargs.key?(:aliases)
+      _orig_safe_load(yaml, **kwargs)
+    end
+  end
+end
+# --- /Psych(YAML) alias enable ---
+
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
