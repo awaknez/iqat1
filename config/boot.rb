58b8392b8b3b9e28722c0d392bde22a26fe57115
awaknez <awaknez@users.noreply.github.com>
2026-01-25 13:08:09 +0900

    Fix deploy: enable YAML aliases for Psych

--- config/boot.rb
+++ config/boot.rb
@@ -3,4 +3,8 @@ require 'logger'  # ← 2025/06/29これを最上部に追加
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+require "psych"
+Psych::DEFAULT_OPTIONS[:aliases] = true if Psych.const_defined?(:DEFAULT_OPTIONS)
+
 require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
