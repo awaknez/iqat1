3224b0e8da94fb090d4c15affe2222eba7a2d059
awaknez <awaknez@users.noreply.github.com>
2026-01-25 13:23:58 +0900

    Enable YAML aliases for Psych to fix webpacker.yml load on Ruby 3.2

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,19 @@ require_relative 'boot'
 
 require 'rails/all'
 
+# --- Psych(YAML) alias enable (Ruby 3.2+) ---
+require "psych"
+module Psych
+  class << self
+    alias_method :_orig_safe_load, :safe_load
+    def safe_load(yaml, **kwargs)
+      kwargs[:aliases] = true unless kwargs.key?(:aliases)
+      _orig_safe_load(yaml, **kwargs)
+    end
+  end
+end
+# --- /Psych(YAML) alias enable ---
+
 require 'logger'  # ← 2025/06/29 この行を追加
 
 # Require the gems listed in Gemfile, including any gems
